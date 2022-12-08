cbd880ed1e6b15bfffc72fa4b03c84e94d0f25ee
brady-goldsworthy <goldsworthybrady@gmail.com>
2022-12-07 20:17:30 -0700

    Rebalance Factorum

--- src/server/cards/promo/Factorum.ts
+++ src/server/cards/promo/Factorum.ts
@@ -34,7 +34,6 @@ export class Factorum extends Card implements IActionCard, ICorporationCard {
               eb.empty().startAction.production((pb) => pb.energy(1)).or();
             });
             ce.action('Increase your energy production 1 step IF YOU HAVE NO ENERGY RESOURCES, or spend 3M€ or 1 energy to draw a building card.', (eb) => {
-              
               eb.energy(1).slash().megacredits(3).startAction.cards(1, {secondaryTag: Tag.BUILDING});
             });
           });
@@ -57,7 +56,7 @@ export class Factorum extends Card implements IActionCard, ICorporationCard {
       },
     );
 
-    const drawBuildingCard = new SelectOption('Spend 3 M€ to draw a building card', 'Draw card', () => {
+    const drawBuildingCardMegaCredits = new SelectOption('Spend 3 M€ to draw a building card', 'Draw card', () => {
       player.payMegacreditsDeferred(
         3,
         'Select how to pay for Factorum action.',
@@ -65,9 +64,33 @@ export class Factorum extends Card implements IActionCard, ICorporationCard {
       return undefined;
     });
 
-    if (player.energy > 0) return drawBuildingCard;
-    if (!(player.canAfford(3) || player.energy >= 1)) return increaseEnergy;
+    const drawBuildingCardEnergy = new SelectOption(
+      'Spend 1 energy to draw a building card',
+      'Draw card',
+      () => {
+        player.deductResource(Resources.ENERGY, 1);
+        player.drawCard(1, {tag: Tag.BUILDING});
+        return undefined;
+      }
+    );
 
-    return new OrOptions(increaseEnergy, drawBuildingCard);
+    const can_increase_energy = player.energy === 0;
+    const can_afford_megacredits = player.canAfford(3);
+    const can_afford_energy = player.energy >= 1;
+
+    if (!can_increase_energy) {
+      if (!can_afford_energy) return drawBuildingCardMegaCredits;
+      if (!can_afford_megacredits) return drawBuildingCardEnergy;
+      return new OrOptions(drawBuildingCardMegaCredits, drawBuildingCardEnergy);
+    }
+    if (!can_afford_megacredits) {
+      if (!can_afford_energy) return increaseEnergy;
+      return new OrOptions(increaseEnergy, drawBuildingCardEnergy);
+    }
+    if (!can_afford_energy) {
+      return new OrOptions(increaseEnergy, drawBuildingCardMegaCredits);
+    }
+
+    return new OrOptions(increaseEnergy, drawBuildingCardMegaCredits, drawBuildingCardEnergy);
   }
 }
