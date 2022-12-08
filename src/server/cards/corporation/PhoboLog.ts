b6177b1427eaca988ff510554328c97f1ce175e3
brady goldsworthy <brady-goldsworthy>
2022-12-07 18:43:34 -0600

    Rebalanced Phobolog

--- src/server/cards/corporation/PhoboLog.ts
+++ src/server/cards/corporation/PhoboLog.ts
@@ -13,19 +13,23 @@ export class PhoboLog extends Card implements ICorporationCard {
       cardType: CardType.CORPORATION,
       name: CardName.PHOBOLOG,
       tags: [Tag.SPACE],
-      startingMegaCredits: 23,
+      startingMegaCredits: 30,
 
       behavior: {
-        stock: {titanium: 10},
+        stock: {titanium: 8},
         titanumValue: 1,
+      },
+
+      firstAction: {
+        text: 'Draw two cards with a space tag',
+        drawCard: {count: 2, tag: Tag.SPACE},
       },
 
       metadata: {
         cardNumber: 'R09',
-        description: 'You start with 10 titanium and 23 M€.',
+        description: 'You start with 8 titanium and 30 M€. As your first action, reveal cards from the deck until you have revealed 2 cards with a space tag. Take them into hand and discard the rest',
         renderData: CardRenderer.builder((b) => {
-          b.br.br;
-          b.megacredits(23).nbsp.titanium(10, {digit});
+          b.megacredits(30).nbsp.titanium(8, {digit}).cards(2, {secondaryTag: Tag.SPACE});
           b.corpBox('effect', (ce) => {
             ce.effect('Your titanium resources are each worth 1 M€ extra.', (eb) => {
               eb.titanium(1).startEffect.plus(Size.SMALL).megacredits(1);
