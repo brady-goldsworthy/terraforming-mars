28f05b48a590c7bf91714b829c1fdf6bd3ab3c86
brady-goldsworthy <goldsworthybrady@gmail.com>
2022-12-11 16:32:11 -0700

    Rebalance Economizer

--- src/server/milestones/Economizer.ts
+++ src/server/milestones/Economizer.ts
@@ -3,11 +3,11 @@ import {Player} from '../Player';
 
 export class Economizer implements IMilestone {
   public readonly name = 'Economizer';
-  public readonly description = 'Requires that you have 5 heat production';
+  public readonly description = 'Requires that you have 8 heat production';
   public getScore(player: Player): number {
     return player.production.heat;
   }
   public canClaim(player: Player): boolean {
-    return this.getScore(player) >= 5;
+    return this.getScore(player) >= 8;
   }
 }
