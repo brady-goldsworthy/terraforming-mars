c03e9e94a58eca39a8fb25fc2b6453c302472c04
brady-goldsworthy <goldsworthybrady@gmail.com>
2023-08-25 08:59:36 -0600

    Rebalance Thorgate

--- src/server/cards/corporation/Thorgate.ts
+++ src/server/cards/corporation/Thorgate.ts
@@ -12,19 +12,19 @@ export class Thorgate extends Card implements ICorporationCard {
       cardType: CardType.CORPORATION,
       name: CardName.THORGATE,
       tags: [Tag.POWER],
-      startingMegaCredits: 48,
+      startingMegaCredits: 38,
 
       behavior: {
-        production: {energy: 1},
+        production: {energy: 3},
       },
 
       cardDiscount: {tag: Tag.POWER, amount: 3},
       metadata: {
         cardNumber: 'R13',
-        description: 'You start with 1 energy production and 48 M€.',
+        description: 'You start with 3 energy production and 38 M€.',
         renderData: CardRenderer.builder((b) => {
           b.br;
-          b.production((pb) => pb.energy(1)).nbsp.megacredits(48);
+          b.production((pb) => pb.energy(3)).nbsp.megacredits(38);
           b.corpBox('effect', (ce) => {
             ce.effect('When playing a power card OR THE STANDARD PROJECT POWER PLANT, you pay 3 M€ less for it.', (eb) => {
               // TODO(chosta): energy().played needs to be power() [same for space()]
